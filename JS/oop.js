7f929c4225502d4ea0fef4b3781646acb2c94e08
Paranetharan Jeevaraj <32712199+codeparane@users.noreply.github.com>
2018-10-11 00:07:33 +0530

    Angular Js Mdoel, Controller, Directve

--- JS/oop.js
+++ JS/oop.js
@@ -6,3 +6,14 @@ let stu1 = {
     grade:3,
     marks:English
 }
+
+Data type 
+Primitative - number, string, boolean 
+Reference Type Object  - Object
+
+// Integer, String, Object, Boolean apram Static x = 10; 
+// int, string, bool,  y = 10;
+
+run(){
+    static 
+}
