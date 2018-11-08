cd2216e030bdb7a711434b9ca0c9beb1c365d09e
Paranetharan Jeevaraj <32712199+codeparane@users.noreply.github.com>
2018-11-08 21:43:26 +0530

    update Notes

---
+++ Algorithms/Codes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 12, 23, 1, 45, 9 };
+            Console.Write("Before Sorting :");
+            printArray(num);
+
+            //Selection Sort
+            for (int i =0; i < num.Length - 1; i++){
+                int minIndx = i;
+                for(int j=i+1;j<num.Length;j++){
+                    if(num[j] < num[minIndx] ){
+                        minIndx = j;
+                    }
+                }
+                int tmp = num[minIndx];
+                num[minIndx] = num[i];
+                num[i] = num[minIndx];
+            }
+            Console.WriteLine();
+            printArray(num);
+
+        }
+
+        static void printArray(int[] _array)
+        {
+            foreach (int item in _array)
+            {
+                Console.Write(" " + item);
+            }
+        }
+    }
+}
