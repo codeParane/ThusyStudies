1ffab46a01e7fde38736bd931e060995b9c008b5
vThushy <40529679+vThushy@users.noreply.github.com>
2018-11-11 20:43:32 +0530

    Pattern Added

---
+++ Exercises/Pattern/Patterns.cs
@@ -0,0 +1,60 @@
+using System;
+public class Patterns
+{
+    public void print1()
+    {
+        //*****
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("*");
+        }
+    }
+
+    public void print2()
+    {
+        /*
+        *
+        *
+        *
+        *
+        *
+        */
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("*");
+        }
+    }
+    public void print3()
+    {
+        /*
+        1
+        12
+        123
+        1234
+        12345
+         */
+        for (int k = 1; k <= 5; k++)
+        {
+            for (int l = 1; l <= k; l++)
+            {
+                Console.Write(l);
+            }
+            Console.WriteLine("");
+        }
+    }
+    public void print4(){
+        /*
+        
+         */
+    }
+
+
+
+
+
+
+
+
+
+}
