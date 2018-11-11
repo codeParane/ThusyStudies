66442c5902b2f2bd4e7d6d309cf33b2ddded34db
vThushy <40529679+vThushy@users.noreply.github.com>
2018-11-11 14:52:23 +0530

    initial commit

---
+++ Exercises/Pattern/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pattern1();
+        }
+
+        static void pattern1()
+        {
+            /*
+            *
+            **
+            ***
+            ****
+            *****
+            */
+        
+        
+
+
+
+        }
+
+        static void pattern2()
+        {
+            /*
+            12345
+            1234
+            123
+            12
+            1
+            */
+
+
+        }
+
+        static void pattern3()
+        {
+            /*
+            13579
+            1357
+            13
+            1
+            */
+        }
+        static void pattern4()
+        {
+            /*
+                *
+               **
+              ***
+             ****
+            *****
+            */
+        }
+        static void pattern5()
+        {
+            /*
+            *****
+             ****
+              ***
+               **
+                *
+            */
+        }
+        static void pattern6()
+        {
+            /*
+            1
+            12
+            123
+            1234
+            12345
+            */
+        }
+        static void pattern7()
+        {
+            /*
+
+            */
+        }
+
+
+
+
+    }
+}
