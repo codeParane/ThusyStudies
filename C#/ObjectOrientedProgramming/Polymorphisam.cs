5ad1c40fd29e41e84d4e03bd4095eacb4f5215f0
vThushy <40529679+vThushy@users.noreply.github.com>
2018-12-01 16:56:00 +0530

    Commit polymorphisam

--- C#/ObjectOrientedProgramming/Polymorphisam.cs
+++ C#/ObjectOrientedProgramming/Polymorphisam.cs
@@ -3,49 +3,54 @@ using System;
 namespace ObjectOrientedProgramming
 {
     //polymorphisam
-    //1 - static, compile time, early binding(method overloading, operator overloading)
+
+    //1 - static, compile time, early binding(method overloading, operator overloading
     //2 - dynamic, runtime, late binding(virtual , overriding method)
 
-    //compile time
-    //same signature, diff types of parameter or diff no of parameters 
-    
+//Static - Method Overloading or functional overloading
+class MethodOverLoading{
+    void printWhatPass(string para){
+        string msg = "Hi welcome to polymorphisam";
+        Console.WriteLine("string pass {0}, {1}", para, msg);
+    }
+    void printWhatPass(double para){
+        Console.WriteLine("Double Pass {0}", para);
+    }
+}
 
-    class Shapes{
-        //initial method - no arg method
-        public int area(){
-            return 1;
-        }
-        
-        //over load with 1 argument
-        public int area(int rad){
-            return (22/7 * rad * rad);
-        }
-
-        //over load with 2 arguments
-        public int area(int hei, int wid){
-            return (hei * wid);
-        }
-        
-
-        //diff type of arg
-        public int area(double rad){
-            
-        }
+//static - operator overloading
+class OperatorOverloading{
+    void diffPara(int one){
+        Console.WriteLine("One para Pass : {0}", one);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    void diffPara(int one, int two){
+        Console.WriteLine("Two para Pass : {0}, {1}", one, two);
+    }
 }
+
+//Dynamic - Overriding
+class Animal{
+    public virtual void print(){
+        Console.WriteLine("This is the Animal Class Print Method");
+    }
+}
+
+class Dog: Animal{
+    public override void print(){
+        Console.WriteLine("This is tha Dog Class Print Method");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}
+    
+    
--- C#/ObjectOrientedProgramming/Program.cs
+++ C#/ObjectOrientedProgramming/Program.cs
@@ -32,14 +32,8 @@ namespace ObjectOrientedProgramming
             */
 
             //Call overloading methods
-            Shapes empty = new Shapes();
-            Console.WriteLine("no arg method called : " + empty.area());
-            //Call overloading methods
-            Shapes circle = new Shapes();
-            Console.WriteLine("1 arg method called : " + circle.area(7));
-            //Call overloading methods
-            Shapes rectangle = new Shapes();
-            Console.WriteLine("2 arg method called : " + rectangle.area(8,4));
+            Dog d1 = new Dog();
+            d1.print();
 
             
 
