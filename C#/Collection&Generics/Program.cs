e2fdd34c6cbbefb99ba2baf99a85b52cbc9b748b
vThushy <40529679+vThushy@users.noreply.github.com>
2019-01-05 22:16:40 +0530

    Update Program.cs

--- C#/Collection&Generics/Program.cs
+++ C#/Collection&Generics/Program.cs
@@ -8,7 +8,7 @@ namespace Collection_Generics
     {
         static void Main(string[] args)
         {
-            /* 
+            
             //Array - (Fixed Length, Type Safe)
             int[] num = {1, 2, 3, 4, 5}; //5 (0-4)
             string[] seasons = {"Winter", "Summer", "Spring", "Autumn"};
@@ -26,6 +26,9 @@ namespace Collection_Generics
             Marks.Add(4);
             Marks.Add(true);   
             Console.Write("\nArrayList After Adding 4 value Capacity : " + Marks.Capacity); 
+
+
+            Object a = Marks[10];
 
             //Hashtable <key, value> Pair
             Hashtable EmpDet = new Hashtable();
@@ -48,8 +51,8 @@ namespace Collection_Generics
             s1.Pop(); //delete and return the top value
 
 
-            */
-            //Generics - (Auto Resize , Type Safe)
+            
+            //Generics - (Auto Resize , Type Safe),
             // List - 
             List<int> L1 = new List<int>();
             Console.Write("Initial Size : " + L1.Capacity);
@@ -57,13 +60,38 @@ namespace Collection_Generics
             // L1.Add("String");   ERROR : cant accept another data types values;
             Console.Write("\nSize After Added 1 value : " + L1.Capacity);
 
+/*
+Insertaion Order, Sorting Order
+Key&value , Value only
 
-            //Dictionary
-            Dictionary<S?> d1 = new Dictionary();
+
+
+ */
 
 
 
+            //Dictionary- Similiar to hashtable in Generics, 
+            //Key, Value pair like as Hashtable, but needs to define the data type
+            Dictionary<int,string> d1 = new Dictionary<int, string>();
+            d1.Add(1,"Thushy");
+            // d1.Add("",""); it will pop the error
+
+            //Sortedlist - Auto Sort by key ASC
+            SortedList<int,string> s1 = new SortedList<int,string>();
+            s1.Add(2,"Banana");
+            s1.Add(1, "Apple");
+
+            // Stack - Generics
+            Stack<int> st1 = new Stack<int>();
+            st1.Push(2);
+
+
+            Queue<double> Qu1 = new Queue<double>();
+            Qu1.Enqueue(2.32);
+            Console.Write(Qu1.Dequeue());
+
+
+            
         }
     }
-}
-//
+} 
